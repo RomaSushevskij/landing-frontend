fe8067d8c19bc7589de315e042f6413fb0ad9c45
RomaSushevskij <roma.sushevskij@yandex.ru>
2023-01-22 19:11:32 +0300

    responsive About Us

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import { Effects } from 'components/Effects/Effects';
-import { Footer } from 'components/Footer';
 import { Header } from 'components/Header';
 import { Substrate } from 'components/Substrate';
-import { AboutUs } from 'screens/AboutUs';
-import { Asks } from 'screens/Asks';
-import { ContactUs } from 'screens/ContactUs';
-import { Gallery } from 'screens/Gallery';
-import { Main } from 'screens/Main';
-import { Review } from 'screens/Review';
 
 import style from './App.module.scss';
 
-import { Steps } from 'screens/Steps';
-import { Technologies } from 'screens/Techhologies';
+import { AboutUs } from 'screens/AboutUs';
+import { Main } from 'screens/Main';
 import { ReturnComponent } from 'types';
 
 const App = (): ReturnComponent => {
@@ -23,14 +15,14 @@ const App = (): ReturnComponent => {
         <Header />
         <Main />
         <AboutUs />
-        <Technologies />
-        <Steps />
-        <Asks />
-        <Review />
-        <Gallery />
-        <ContactUs />
-        <Footer />
-        <Effects />
+        {/* <Technologies /> */}
+        {/* <Steps /> */}
+        {/* <Asks /> */}
+        {/* <Review /> */}
+        {/* <Gallery /> */}
+        {/* <ContactUs /> */}
+        {/* <Footer /> */}
+        {/* <Effects /> */}
       </div>
     </div>
   );
